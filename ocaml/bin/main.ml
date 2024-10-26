6541c5afc517ebd88a867a9b6af6c52d155af1b1
Sebastiano Rebonato-Scott <141144328+all-c-a-p-s@users.noreply.github.com>
2024-10-26 09:47:38 +0200

    day 1 part 2 in ocaml

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,2 +1,5 @@
 let () =
-  Printf.printf "day 1 part 1: %d\n" ("lib/input_01.txt" |> Aoc.Day_01.answer)
+  Printf.printf "day 1 part 1: %d\n" ("lib/input_01.txt" |> Aoc.Day_01.part_one)
+
+let () =
+  Printf.printf "day 1 part 2: %d\n" ("lib/input_01.txt" |> Aoc.Day_01.part_two)
--- ocaml/lib/day_01.ml
+++ ocaml/lib/day_01.ml
@@ -15,6 +15,42 @@ let char_to_digit c =
   if c >= '0' && c <= '9' then int_of_char c - int_of_char '0'
   else failwith "tried to convert non-digit to int"
 
+let rec last_number reversed_line =
+  if is_digit reversed_line.[0] then char_to_digit reversed_line.[0]
+  else
+    match reversed_line with
+    | s when String.starts_with s ~prefix:"eno" -> 1
+    | s when String.starts_with s ~prefix:"owt" -> 2
+    | s when String.starts_with s ~prefix:"eerht" -> 3
+    | s when String.starts_with s ~prefix:"ruof" -> 4
+    | s when String.starts_with s ~prefix:"evif" -> 5
+    | s when String.starts_with s ~prefix:"xis" -> 6
+    | s when String.starts_with s ~prefix:"neves" -> 7
+    | s when String.starts_with s ~prefix:"thgie" -> 8
+    | s when String.starts_with s ~prefix:"enin" -> 9
+    | s when String.starts_with s ~prefix:"orez" -> 0
+    | u when String.length u > 0 ->
+        last_number (String.sub u 1 (String.length u - 1))
+    | _ -> failwith "failde to find last number"
+
+let rec first_number line =
+  if is_digit line.[0] then char_to_digit line.[0]
+  else
+    match line with
+    | s when String.starts_with s ~prefix:"one" -> 1
+    | s when String.starts_with s ~prefix:"two" -> 2
+    | s when String.starts_with s ~prefix:"three" -> 3
+    | s when String.starts_with s ~prefix:"four" -> 4
+    | s when String.starts_with s ~prefix:"five" -> 5
+    | s when String.starts_with s ~prefix:"six" -> 6
+    | s when String.starts_with s ~prefix:"seven" -> 7
+    | s when String.starts_with s ~prefix:"eight" -> 8
+    | s when String.starts_with s ~prefix:"nine" -> 9
+    | s when String.starts_with s ~prefix:"zero" -> 0
+    | u when String.length u > 0 ->
+        first_number (String.sub u 1 (String.length u - 1))
+    | _ -> failwith "failed to find first number"
+
 let line_score line =
   let l = line |> String.to_seq |> List.of_seq in
   let digits = List.filter is_digit l in
@@ -23,9 +59,22 @@ let line_score line =
 
 let rec total_score lines =
   match lines with [] -> 0 | x :: xs -> line_score x + total_score xs
-(*I think this syntax is pretty nice - if the list is now empty then it is
-  equivalent to some first element x :: some rest of list xs which may or may not be empty*)
 
-let answer filename =
+let rev x =
+  let len = String.length x in
+  String.init len (fun n -> String.get x (len - n - 1))
+
+let line_score_part_two line = (first_number line * 10) + last_number (rev line)
+
+let part_one filename =
   let lines = read_lines filename in
   total_score lines
+
+let rec total_part_two lines =
+  match lines with
+  | [] -> 0
+  | x :: xs -> line_score_part_two x + total_part_two xs
+
+let part_two filename =
+  let lines = read_lines filename in
+  total_part_two lines
